de56b9919f7d3031c032c202b7168c988a0eb5f0
Peter Till <tillpetya20@gmail.com>
2023-11-05 15:08:25 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const net = require('net');
 const crypto = require('crypto');
 const axios = require('axios');
-const prompt = require('prompt-sync')();
+
 
 class Block {
-    constructor(index, transactions, timestamp, previousHash, nonce = 0, data = [{}]) {
+    constructor(index, transactions, timestamp, previousHash, nonce = 0, data = {}, publicKey = "") {
         this.index = index;
         this.transactions = transactions;
 		this.data = data;
@@ -11,90 +12,226 @@ class Block {
         this.previousHash = previousHash;
         this.nonce = nonce;
 		this.previousHash = previousHash;
+        this.publicKey = publicKey;
     }
 
     computeHash() {
         const blockString = JSON.stringify(this, Object.keys(this).sort());
 		return crypto.createHash('sha256').update(blockString).digest('hex');
     }
+
+    toJSON() {
+        return {
+          index: this.index,
+          transactions: this.transactions,
+          timestamp: this.timestamp,
+          previousHash: this.previousHash,
+          nonce: this.nonce,
+          data: this.data,
+          publicKey: this.publicKey
+        };
+    }
+    static fromJSON(json) {
+		const { index, transactions, timestamp, previousHash, nonce, data, publicKey } = json;
+		return new Block(index, transactions, timestamp, previousHash, nonce, data, publicKey);
+	}
 }
 
-class Miner {
-    constructor(serverUrl, minerAddress) {
-        this.serverUrl = serverUrl;
-        this.minerAddress = minerAddress;
-        console.log("\x1b[33m Welcome to CodersCoin Miner! \x1b[0m");
-        console.log("\n");
-        console.log(" -------------------------------------------- ");
-        console.log("\n");
+// ANSI escape codes for background colors
+const backgroundColors = {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+    reset: '\x1b[0m' // Reset to default color
+};
+
+function colorText(text, color) {
+    return backgroundColors[color] + text + ' ' + backgroundColors.reset;
+}
+
+console.log("\x1b[33m Welcome to CodersCoin Miner! \x1b[0m");
+console.log("\n");
+console.log(" -------------------------------------------- ");
+console.log("\n");
+
+
+
+
+function proofofwork(block) {
+    block.nonce = 0;
+    let computedHash = block.computeHash();
+    const difficulty = 2; // Assuming 'difficulty' is a property of the block object
+
+    while (!computedHash.startsWith('0'.repeat(difficulty))) {
+        block.nonce++;
+        computedHash = block.computeHash();
     }
 
-    async mine() {
-        console.log("Looking for a block to mine...");
-        try {
-            const dt = await axios.get(`${this.serverUrl}/api/get/latestblock`);
-            const block = dt.data;
+    console.log(colorText("CLIENT", "magenta"), "Mining with difficulty: ", block.nonce, "...");
+    return computedHash;
+}
 
-            const mineableBlock = new Block(block.index, block.transactions, block.timestamp, block.previousHash,block.nonce, block.data);
-            console.log("\x1b[34m Received a new job: ", block.index, "\x1b[0m");
-            const latestHash = mineableBlock.computeHash();
-            console.log("Latest hash: ", latestHash);
+function isValidProof(block, blockHash) {
+    return blockHash.startsWith('0'.repeat(2)) && blockHash === block.computeHash();
+}
 
-            const now = Math.floor(Date.now() / 1000);
-            const newBlock = new Block(mineableBlock.index + 1, mineableBlock.transactions, now, latestHash);
-            const proof = this.proofofwork(newBlock);
-            console.log("Proof:", proof);
+const PEERLIST = [
+	{host:"localhost", port:3000}
+];
 
-            if (!this.isValidProof(newBlock, proof)) {
-                console.log("\x1b[31m Not valid proof \x1b[0m");
-            } else {
-                console.log("\x1b[92m Proof seems to be valid! \x1b[0m");
-            }
+(async function() {
+	try {
+		await peerDiscovery();
+	} catch (err) {
+		console.log("Cannot connect to peers: ", err.message);
+	}
+})();
 
-            const crafted = this.craftResponse(latestHash, [mineableBlock.index + 1, mineableBlock.transactions, now, latestHash], proof, this.minerAddress);
+// PEER DISCOVERY FUNCTIONS
 
-            const response = await axios.post(`${this.serverUrl}/mine/`, crafted);
-            if (response.status === 200) {
-                console.log("Block mined and added to the blockchain!");
-                prompt("Press enter to mine again...");
-            } else {
-                console.log("\x1b[31m Mining failed. \x1b[0m");
-            }
-        } catch (error) {
-            console.error("\x1b[31m An error occurred during mining:", error.message, " CSC has different meanings for error messages, check the README file on Github! \x1b[0m");
-            prompt("Press enter to mine again...");
-        }
+
+
+async function peerDiscovery() {
+	const randomPeer = await getRandomPeer();
+
+	console.log(randomPeer);
+
+	if (randomPeer) {
+		const { host, port } = randomPeer;
+		const client = net.connect(port, host, () => {
+			client.write(JSON.stringify({
+				type: 'getPeers', 
+				requester: {
+					host: "wallet", // Your own host
+					port: 3000, // Your own port
+			  	}
+			}));
+		});
+
+		client.on('data', (data) => {
+			const receivedData = JSON.parse(data);
+			if (receivedData.type === 'sendPeers') {
+			  // Update your list of known peers with the information received
+			  const newPeers = receivedData.peers;
+			  newPeers.forEach((peer) => {
+				if (!PEERLIST.some((knownPeer) => knownPeer.host === peer.host && knownPeer.port === peer.port)) {
+						PEERLIST.push(peer);
+				}
+			  });
+			  console.log('Updated the list of known peers!');
+	  
+			  // Attempt to connect to the newly discovered peers
+			  
+			}
+			client.end();
+		  });
+	  
+		  client.on('error', (error) => {
+			console.error('Error during peer discovery: Could not connect to seed peer');
+		  });
+	}
+}
+
+function removePeer(peerSocket) {
+	const peerIndex = connectedPeers.indexOf(peerSocket);
+	if (peerIndex !== -1) {
+		connectedPeers.splice(peerIndex, 1);
+	}
+}
+
+async function getRandomPeer() {
+	const randomIndex = Math.floor(Math.random() * PEERLIST.length);
+
+	return PEERLIST[randomIndex];
+}
+
+function broadcastBlocks(latestBlock, newBlock, proof, minerUser, peer) {
+    const data = {
+        latestblock: latestBlock,
+        newblock: newBlock,
+        proof: proof,
+        miner: minerUser
     }
+    console.log(colorText("NETWORK", "yellow"), " Submitting the new block to the network...");
+    const client = net.connect(peer.port, peer.host, () => {
+        console.log(colorText("NETWORK", "blue"), "Connected to peer:", peer.host + ':' + peer.port);
+        const requestData = { type: 'newBlock', blocks: data };
+        client.write(JSON.stringify(requestData));
+    });
+    client.on('error', error => {
+		broadcastBlocks(latestBlock, newBlock, proof, minerUser, getRandomPeer());
+	});
 
-    proofofwork(block) {
-        block.nonce = 0;
-        let computedHash = block.computeHash();
-        const difficulty = 2; // Assuming 'difficulty' is a property of the block object
-
-        while (!computedHash.startsWith('0'.repeat(difficulty))) {
-            block.nonce++;
-            computedHash = block.computeHash();
-        }
-		console.log("\x1b[93m Mining with difficulty: ", block.nonce, "... \x1b[0m")
-        return computedHash;
-    }
-
-    isValidProof(block, blockHash) {
-        return blockHash.startsWith('0'.repeat(2)) && blockHash === block.computeHash();
-    }
-
-    craftResponse(latestHash, newBlock, proof, miner) {
-		console.log("\x1b[93m Submitting block to the network... \x1b[0m");
-        const data = {
-            latesthash: latestHash,
-            proof: proof,
-            newblock: newBlock,
-            miner: miner
-        };
-        return data;
-    }
 }
 
 // Usage example
-const miner = new Miner("https://csc.onrender.com", prompt('What is your username? > '));
-miner.mine();
+//const miner = new Miner("https://csc.onrender.com", prompt('What is your username? > '));
+//miner.mine();
+//MINER: requests "mineRequest" receives "mineResponse" then returns "newBlock"
+
+async function requestTransaction(peer) {
+    const client = net.connect(peer.port, peer.host, () => {
+        console.log(colorText("NETWORK", "blue"), "Connected to peer:", peer.host + ':' + peer.port);
+        const requestData = { type: 'mineRequest' };
+        client.write(JSON.stringify(requestData));
+    });
+    client.on('data', async data => {
+        const receivedData = JSON.parse(data.toString());
+        if (receivedData.type === 'mineResponse') {
+            tx = receivedData.transaction;
+            if(!tx) {
+                console.log('Transaction pool is empty. Requesting a new peer...', receivedData);
+                await requestTransaction(await getRandomPeer());
+                return;
+            }
+
+            //Get latest blocks hash
+            const latestBlockRaw = receivedData.latestBlock;
+            const latestBlock = Block.fromJSON(latestBlockRaw);
+
+            console.log(colorText("NETWORK", "yellow"), " Received a new job: ", latestBlock.index +1);
+
+            const latestHash = latestBlock.computeHash();
+
+            const now = Math.floor(Date.now() / 1000);
+            const firsttransaction = tx.transaction;
+
+            const newBlock = new Block(latestBlock.index + 1, [
+                firsttransaction,
+                { fromAddress:firsttransaction.fromAddress, toAddress:tx.node, amount:(firsttransaction.amount * 0.15) / 2 },
+                { fromAddress:firsttransaction.fromAddress, toAddress:"mineruser", amount:(firsttransaction.amount * 0.15) / 2 },
+            ], now, latestHash, 0, {}, tx.publicKey);
+
+            const proof = proofofwork(newBlock);
+            console.log("Proof:", proof);
+
+            if (!isValidProof(newBlock, proof)) {
+                console.log(colorText("CLIENT", "red"), "Invalid proof!");
+            } else {
+                console.log(colorText("CLIENT", "green"), "Proof seems to be valid!");
+            }
+
+            broadcastBlocks(latestBlock.toJSON(), newBlock.toJSON(), proof, "mineruser", {host:"localhost", port:3000});
+
+
+        }else {
+            client.end();
+            return false;
+        }
+        
+    });
+}
+
+const miningInterval = 10000; // Set the interval in milliseconds (e.g., 60 seconds)
+
+(async function() {
+    setInterval(async () => {
+        await requestTransaction(await getRandomPeer());
+    }, miningInterval);
+})();
+
